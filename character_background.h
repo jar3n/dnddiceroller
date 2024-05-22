158fd7019e2374fdfede0bb516ba8f83219f999b
James Englander <jenglander@mybuddy.lan>
2024-05-21 23:12:10 -0400

    Created background class

---
+++ character_background.h
@@ -0,0 +1,30 @@
+#ifndef _CHARACTER_BACKGROUND_H_
+#define _CHARACTER_BACKGROUND_H_
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class Background {
+    public:
+    Background();
+    ~Background();
+
+    
+
+    private:
+    // proficiencies use the same int 
+    // as defined in character class
+    int proficiencies_gained;
+    // some backgrounds have options 
+    // for the tools you get
+    vector<string> tools;
+    vector<string> tool_options;
+
+    // backgrounds come with some gold
+    int gold;
+
+};
+
+#endif
