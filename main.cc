f91359b8e4ecadbca5fcae386d436c4bb26f4891
James Englander <jenglander@mybuddy.lan>
2024-05-24 21:22:48 -0400

    Added test for rolldx to main.cc

--- main.cc
+++ main.cc
@@ -17,13 +17,7 @@ int main(int argc, char * argv[]){
     // with a way to safely get out
     // have a function that prompts for a character file
     // have a function that generates a random number
-    srand(time(nullptr));
-    int rand_val = rand();
-    int d20_rand = (rand() % 20) + 1;
-
     cout << "HELLO WORLD\n";
-    cout << "This is a random number: " << rand_val << "\n";
-    cout << "This is a random d20 roll: " << d20_rand << "\n";
 
     Dice * mydice = new Dice();
 
@@ -34,6 +28,7 @@ int main(int argc, char * argv[]){
     cout << "This is a d12 from my dice class: " << mydice->rolld12() << "\n";
     cout << "This is a d20 from my dice class: " << mydice->rolld20() << "\n";
     cout << "This is a d100 from my dice class: " << mydice->rolld100() << "\n";
+    cout << "This is a dX from my dice class with cap of 1000: " << mydice->rolldX(1000) << "\n";
 
     string charfile("/home/jenglander/dndcode/testfiles/chartemp.json");
     cout << "Reading in the following character file: " << charfile << "\n";
