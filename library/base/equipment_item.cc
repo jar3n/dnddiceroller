500b526762a3b007d5af0b30afaaecc6d2d72d0d
James Englander <jenglander@mybuddy.lan>
2024-05-27 11:40:41 -0400

    Expanded base equipment item class with magic and equippable subclasses

--- library/base/equipment_item.cc
+++ library/base/equipment_item.cc
@@ -9,54 +9,145 @@ EquipmentItem::EquipmentItem(string name,
                              name(name),
                              cost(cost),
                              weight(weight),
-                             description(description),
-                             attuned(false),
-                             equipped(false)
+                             description(description)
 {
 }
 
-EquipmentItem::EquipmentItem(string name, 
-                             int cost, 
-                             int weight,
-                             string description,
-                             bool equipped) :
-                             name(name),
-                             cost(cost),
-                             weight(weight),
-                             description(description),
-                             equipped(equipped),
-                             attuned(false)
+string EquipmentItem::getDescription()
+{
+    string equipmentDescription = name + ":\n";
+    equipmentDescription += "Cost: " + to_string(cost) + "\n";
+    equipmentDescription += "Weight: " + to_string(weight) + "\n";
+    equipmentDescription += description + "\n";
+    return equipmentDescription;
+}
+
+EquippableItem::EquippableItem(string name, 
+                               int cost, 
+                               int weight, 
+                               string description, 
+                               bool equipped = false) :
+                               EquipmentItem(name, cost, weight, description),
+                               equipped(equipped)
+
 {
 }
 
-EquipmentItem::EquipmentItem(string name, 
+void EquippableItem::equip()
+{
+    if(equipped){
+        throw EquipmentException(name, "is already equipped.");
+    } else {
+        equipped = true;
+    }
+}
+
+string EquippableItem::getDescription()
+{
+    
+    string equippedStatus = equipped ? "yes" : "no";
+    string equipmentDescription = name + ":\n";
+    equipmentDescription += "Cost: " + to_string(cost) + "\n";
+    equipmentDescription += "Weight: " + to_string(weight) + "\n";
+    equipmentDescription += "Equipped: " + equippedStatus + "\n";
+    equipmentDescription += description + "\n";
+    return equipmentDescription;
+}
+
+MagicItem::MagicItem(string name, 
                              int cost, 
-                             int weight,
+                             int weight, 
                              string description, 
-                             bool equipped, 
-                             bool attuned) :
-                             name(name),
-                             cost(cost),
-                             weight(weight),
-                             description(description),
-                             equipped(equipped),
+                             bool equipped = false, 
+                             bool attuned = false) :
+                             EquippableItem(name, cost, weight, description, equipped),
                              attuned(attuned)
 {
     if (!equipped && attuned){
-        throw EquipmentException(name, "the item must be equipped to be attuned.");
+        throw EquipmentException(name, "cannot be attuned when not equipped.");
     }
 }
 
-void EquipmentItem::attune()
+void MagicItem::attune()
 {
-    if(equipped){
-        attuned = true;
+    if (!equipped){
+        throw EquipmentException(name, "cannot be attuned when not equipped.");
     } else {
-        throw EquipmentException(name, "the item must be equipped to be attuned.");
+        attuned = true;
     }
 }
 
-void EquipmentItem::equip()
+string MagicItem::getDescription()
 {
-    equipped = true;
+    string equippedStatus = equipped ? "yes" : "no";
+    string attunedStatus = attuned ? "yes" : "no";
+    string equipmentDescription = name + ":\n";
+    equipmentDescription += "Cost: " + to_string(cost) + "\n";
+    equipmentDescription += "Weight: " + to_string(weight) + "\n";
+    equipmentDescription += "Equipped: " + equippedStatus + "\n";
+    equipmentDescription += "Attuned: " + attunedStatus + "\n";
+    equipmentDescription += description + "\n";
+    return equipmentDescription;
+}
+
+MagicItemWithCharges::MagicItemWithCharges(string name, 
+                                           int cost, 
+                                           int weight, 
+                                           string description, 
+                                           int numCharges = 1, 
+                                           bool canRecharge = true, 
+                                           bool equipped = false, 
+                                           bool attuned = false) :
+                                           MagicItem(name, cost, weight, description, equipped, attuned),
+                                           numCharges(numCharges),
+                                           canRecharge(canRecharge)
+{
+    if(numCharges <= 0){
+        throw EquipmentException(name, "must have at least one charge.");
+    } 
+}
+
+void MagicItemWithCharges::useCharge()
+{
+    if(numCharges > 0) {
+        numCharges -= 1;
+    } else {
+        throw EquipmentException(name, "has no more charges to expend.");
+    }
+}
+
+void MagicItemWithCharges::refillCharge()
+{
+    if(canRecharge){
+        if(numCharges == maxNumCharges){
+            throw EquipmentException(name, "at full charge.");
+        } else {
+            numCharges++;
+        }
+    } else {
+        throw EquipmentException(name, "cannot recharge.");
+    }
+}
+
+void MagicItemWithCharges::refillAllCharges()
+{
+    if(canRecharge){
+        numCharges = maxNumCharges;
+    } else {
+        throw EquipmentException(name, "cannot recharge.");
+    }
+}
+
+string MagicItemWithCharges::getDescription()
+{
+    string equippedStatus = equipped ? "yes" : "no";
+    string attunedStatus = attuned ? "yes" : "no";
+    string equipmentDescription = name + ":\n";
+    equipmentDescription += "Cost: " + to_string(cost) + "\n";
+    equipmentDescription += "Weight: " + to_string(weight) + "\n";
+    equipmentDescription += "Equipped: " + equippedStatus + "\n";
+    equipmentDescription += "Attuned: " + attunedStatus + "\n";
+    equipmentDescription += "Number of Charges: " + to_string(numCharges) + "\n"; 
+    equipmentDescription += description + "\n";
+    return equipmentDescription;
 }
--- library/base/equipment_item.h
+++ library/base/equipment_item.h
@@ -16,23 +16,96 @@ class EquipmentException : public exception {
 };
 
 class EquipmentItem {
-    private:
+    protected:
     string name;
     int cost;
     int weight;
     string description;
-    bool attuned;
+
+    public:
+    EquipmentItem(string name, 
+                  int cost, 
+                  int weight, 
+                  string description);
+    
+    virtual string getDescription();
+
+};
+
+class EquippableItem : public EquipmentItem {
+    protected:
     bool equipped;
 
     public:
-    EquipmentItem(string name, int cost, int weight, string description);
-    EquipmentItem(string name, int cost, int weight, string description, bool equipped);
-    EquipmentItem(string name, int cost, int weight, string description, bool equipped, bool attuned);
+    EquippableItem(string name,
+                   int cost,
+                   int weight,
+                   string description, 
+                   bool equipped = false);
+
+    void equip();
+    virtual string getDescription();
+};
+
+class MagicItem : public EquippableItem {
+    protected:
+    bool attuned;
+
+    public:
+    MagicItem(string name,
+                   int cost,
+                   int weight,
+                   string description, 
+                   bool equipped = false,
+                   bool attuned = false);
     
     void attune();
-    void equip();
+    virtual string getDescription();
+};
+
+class MagicItemWithCharges : public MagicItem {
+    private:
+    int numCharges;
+    int maxNumCharges;
+    bool canRecharge;
+
+    public:
+    MagicItemWithCharges(string name,
+                         int cost,
+                         int weight,
+                         string description,
+                         int numCharges = 1, 
+                         bool canRecharge = true,
+                         bool equipped = false,
+                         bool attuned = false);
+
+    void useCharge();
+    void refillCharge();
+    void refillAllCharges();
+    virtual string getDescription();
 
 };
+
+// magic items that do not have special properties
+typedef MagicItem Ring;
+typedef MagicItem Rod;
+typedef MagicItemWithCharges Staff;
+typedef MagicItemWithCharges Wand;
+typedef MagicItem Scroll;
+typedef MagicItem WonderousItem;
+
+
+// add the following kinds of items
+// armor
+// magic and not 
+// weapon
+// magic and not
+// there are other types of magic items, but they arent special like 
+// armor or weapons so they can be types basically
+// despite some potions being magical items none are attunable so i will not list them here
+
+
+
 
 
 #endif
