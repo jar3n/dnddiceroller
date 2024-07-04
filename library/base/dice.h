5eb2b8355bd329c8bba58a1d9f19aac2e5512bfc
JREnglander <jenglander25@gmail.com>
2024-07-04 14:52:53 -0400

    Removed dnd namespace from dice

--- library/base/dice.h
+++ library/base/dice.h
@@ -5,34 +5,31 @@
 
 using namespace std;
 
-namespace dnd {
+class Dice {
+    public:
+        Dice();
+        int rolld4();
+        int rolld6();
+        int rolld8();
+        int rolld10();
+        int rolld12();
+        int rolld20();
+        int rolld100();
+        int rolldX(int cap);
 
-    class Dice {
-        public:
-            Dice();
-            int rolld4();
-            int rolld6();
-            int rolld8();
-            int rolld10();
-            int rolld12();
-            int rolld20();
-            int rolld100();
-            int rolldX(int cap);
+    private:
+        random_device rd;
+        mt19937 gen;
+        uniform_int_distribution<> dis4;
+        uniform_int_distribution<> dis6;
+        uniform_int_distribution<> dis8;
+        uniform_int_distribution<> dis10;
+        uniform_int_distribution<> dis12;
+        uniform_int_distribution<> dis20;
+        uniform_int_distribution<> dis100;
 
-        private:
-            random_device rd;
-            mt19937 gen;
-            uniform_int_distribution<> dis4;
-            uniform_int_distribution<> dis6;
-            uniform_int_distribution<> dis8;
-            uniform_int_distribution<> dis10;
-            uniform_int_distribution<> dis12;
-            uniform_int_distribution<> dis20;
-            uniform_int_distribution<> dis100;
-    
-    };
+};
 
-}
 
 
 
--- main.cc
+++ main.cc
@@ -1,6 +1,10 @@
+#include "library/base/dice.h"
 
 
 int main(int argc, char * argv[]){
+    Dice *d = new Dice();
+
+
 
     return 0;
 }   
