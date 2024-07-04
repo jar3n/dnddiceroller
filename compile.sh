f50c7d68390b8508d578dd218e194ffa386bee5d
JREnglander <jenglander25@gmail.com>
2024-07-04 14:50:35 -0400

    Wrote initial character proto file

--- compile.sh
+++ compile.sh
@@ -18,11 +18,20 @@ while getopts "c" arg; do
 done
 
 DNDHOME=$HOME/dndcode
+DNDLIB=$DNDHOME/library
 
 if [[ clean -eq 1 ]]; then
     rm -r $DNDHOME/build
 else
     mkdir $DNDHOME/build
-    g++ -std=c++11 -o $DNDHOME/build/dndhello -include  library/base/weapon.h library/base/weapon.cc library/base/skill.h library/base/skill.cc  library/base/dice.h library/base/dice.cc main.cc -l boost_program_options
+    protoc -I=$DNDHOME/library/serial/characterbook.proto --cpp_out=$DNDHOME/library/serial/
+    g++ -std=c++11 -o $DNDHOME/build/dndhello -include \
+    library/base/skill.h \
+    library/base/skill.cc \
+    library/base/dice.h \
+    library/base/dice.cc \
+    build/characterbook.pb.h \
+    build/characterbook.pb.cc \
+    main.cc -l boost_program_options
 fi
 
