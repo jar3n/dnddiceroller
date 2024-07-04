114c0161bf1259e920de66b7039e9aab703bd06d
JREnglander <jenglander25@gmail.com>
2024-07-04 13:33:46 -0400

    Cleaned up compile script

--- compile.sh
+++ compile.sh
@@ -23,6 +23,14 @@ if [[ clean -eq 1 ]]; then
     rm -r $DNDHOME/build
 else
     mkdir $DNDHOME/build
-    g++ -std=c++11 -o $DNDHOME/build/dndhello -include  library/base/weapon.h library/base/weapon.cc library/base/skill.h library/base/skill.cc  library/base/dice.h library/base/dice.cc main.cc -l boost_program_options
+    g++ -std=c++11 -o $DNDHOME/build/dndhello -include  \
+        library/base/weapon.h \
+        library/base/weapon.cc \
+        library/base/skill.h \
+        library/base/skill.cc  \
+        library/base/dice.h \
+        library/base/dice.cc \
+        main.cc \
+        -l boost_program_options
 fi
 
