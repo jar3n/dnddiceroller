58c0f9058334669f6e8b2ee3026811ea99ad8a7e
JREnglander <jenglander25@gmail.com>
2024-07-07 15:56:55 -0400

    Added ability scores to character prompt and cmdline interface

--- library/serial/ledger.cc
+++ library/serial/ledger.cc
@@ -1,40 +1,6 @@
 #include "ledger.h"
 #include <boost/algorithm/string.hpp>
 
-void ledger::createCharacterHelper()
-{
-    string name, short_name;
-    cout << "Enter the Full Name for your Character: ";
-    getline(cin, name);
-    if (name.empty()){
-        cout << "Character must have a name.\n";
-        return;
-    } 
-    cout << "Enter a Short Name for your Character with no spaces: ";
-    getline(cin, short_name);
-    if (short_name.empty()){
-        cout << "Character must have a Short Name. Aborting current character creation.\n";
-        return;
-    } else if (short_name.find(" ") != string::npos){
-        cout << "The Short Name must have no spaces. Aborting current character creation.\n";
-        return;
-    }
-    int strength;
-    cout << "Enter the Character's Strength Ability(integer): ";
-    cin >> strength;
-    if(cin.fail()){
-        cin.clear();
-        cout << "Received an invalid value for the Strength Ability. Aborting current character creation.\n";
-        return;
-    }
-    cin.ignore(256, '\n');
-    cout << "Adding a character named: " << name << "\n";
-    dnd::character * new_character = _ledger_data.add_characters();
-    new_character->set_name(name);
-    new_character->set_strength(strength);
-    new_character->set_short_name(boost::algorithm::to_lower_copy(short_name));
-}
-
 ledger::ledger(string ledger_path)
 {
     char resolved_path[PATH_MAX];
@@ -63,8 +29,48 @@ ledger::~ledger()
     }
 }
 
-void ledger::createCharacter()
-{
+void ledger::setAbilityScoreHelper(dnd::character *c){
+    int ability_score;
+    for (size_t i = 0; i < NUM_ABILITY_SCORES; i++){   
+        cout << "Enter the Character's " << string(ABILITY_NAMES[i]) << " Ability(integer): ";
+        cin >> ability_score;
+        if(cin.fail()){
+            cin.clear();
+            cout << "Received an invalid value for the " << string(ABILITY_NAMES[i]) << " Ability. Aborting current character creation.\n";
+            return;
+        }
+        c->add_ability_scores(ability_score);
+    }
+}
+
+void ledger::createCharacterHelper(){
+    string name, short_name;
+    dnd::character * new_character = _ledger_data.add_characters();
+    cout << "Enter the Full Name for your Character: ";
+    getline(cin, name);
+    if (name.empty()){
+        cout << "Character must have a name.\n";
+        return;
+    } 
+    cout << "Enter a Short Name for your Character with no spaces: ";
+    getline(cin, short_name);
+    if (short_name.empty()){
+        cout << "Character must have a Short Name. Aborting current character creation.\n";
+        return;
+    } else if (short_name.find(" ") != string::npos){
+        cout << "The Short Name must have no spaces. Aborting current character creation.\n";
+        return;
+    }
+    setAbilityScoreHelper(new_character);
+    cin.ignore(256, '\n'); // at end of
+    cout << "Adding a character named: " << name << "\n";
+    new_character->set_name(name);
+    new_character->set_short_name(boost::algorithm::to_lower_copy(short_name));
+
+
+}
+
+void ledger::createCharacter(){
     if (createdCharacterInConstructor){
         return;
     }
@@ -87,37 +93,76 @@ void ledger::createCharacter()
     }
 }
 
-void ledger::getCharacterInfo(string name)
-{
+void ledger::getCharacterAbilityScores(string name){
+    size_t numAbilityScores = 6;
+    dnd::character c;
+    getCharacter(name, c);
+    cout << c.name() << "'s Ability Scores:\n";
+    for(size_t i = 0; i < NUM_ABILITY_SCORES; i++){
+        cout << "  " << string(ABILITY_NAMES[i]) << ": " << c.ability_scores(i) << "\n";
+    }
+}
+
+void ledger::getCharacter(string name, dnd::character &character){
     name = boost::algorithm::to_lower_copy(name);
     bool found_no_character = true;
     for(int i = 0; i < _ledger_data.characters_size(); i++){
-        dnd::character c = _ledger_data.characters(i);
-        if (c.short_name() == name){
-            cout << c.name() << ": " << endl;
-            cout << "    Strength: " << c.strength() << endl;
+        if (_ledger_data.characters(i).short_name() == name){
+            character = _ledger_data.characters(i);
             found_no_character = false;
             break;
         }
     }
     if (found_no_character){
-        cout << "Found no character with the name: " << name << "\n"; 
+        throw ledger_exception("Found no character with the name: " + name +"\n"); 
     }
 }
 
-void ledger::listCharacters()
-{
+int32_t ledger::strength(string name){
+    dnd::character c;
+    getCharacter(name, c);
+    return c.ability_scores(STRENGTH);
+}
+
+int32_t ledger::dexterity(string name){
+    dnd::character c;
+    getCharacter(name, c);
+    return c.ability_scores(DEXTERITY);
+}
+
+int32_t ledger::constitution(string name){
+    dnd::character c;
+    getCharacter(name, c);
+    return c.ability_scores(CONSTITUTION);
+}
+
+int32_t ledger::intelligence(string name){
+    dnd::character c;
+    getCharacter(name, c);
+    return c.ability_scores(INTELLIGENCE);
+}
+
+int32_t ledger::wisdom(string name){
+    dnd::character c;
+    getCharacter(name, c);
+    return c.ability_scores(WISDOM);
+}
+
+int32_t ledger::charisma(string name){
+    dnd::character c;
+    getCharacter(name, c);
+    return c.ability_scores(CHARISMA);
+}
+
+void ledger::listCharacters(){
     cout << "Available Characters:" << endl;
     for (int i = 0; i < _ledger_data.characters_size(); i++){
-        cout << _ledger_data.characters(i).name() << endl;
+        cout << _ledger_data.characters(i).name() << " (" << _ledger_data.characters(i).short_name() << ")" << endl;
     }
 }
 
-ledger_exception::ledger_exception(string msg) : _msg(msg)
-{
-}
+ledger_exception::ledger_exception(string msg) : _msg(msg){}
 
-string ledger_exception::what()
-{
+string ledger_exception::what(){
     return "Encountered the following issue with the ledger: " + _msg;
 }
--- library/serial/ledger.h
+++ library/serial/ledger.h
@@ -8,6 +8,23 @@
 
 using namespace std;
 
+#define NUM_ABILITY_SCORES 6
+#define ABILITY_NAMES ((const char *[]){"Strength", \
+                                        "Dexterity", \
+                                        "Constitution", \
+                                        "Intelligence", \
+                                        "Wisdom", \
+                                        "Charisma"})
+
+enum ability_score {
+    STRENGTH = 0,
+    DEXTERITY = 1,
+    CONSTITUTION = 2,
+    INTELLIGENCE = 3,
+    WISDOM = 4,
+    CHARISMA = 5
+};
+
 class ledger_exception : public exception {
     private:
         string _msg;
@@ -24,14 +41,25 @@ class ledger {
     dnd::character_ledger _ledger_data;
     bool createdCharacterInConstructor = false;
 
+    void setAbilityScoreHelper(dnd::character *c);
     void createCharacterHelper();
+    void getCharacter(string name, dnd::character& character);
+
+    // helper functions for getting character ability scores
+    int32_t strength(string name);
+    int32_t dexterity(string name);
+    int32_t constitution(string name);
+    int32_t intelligence(string name);
+    int32_t wisdom(string name);
+    int32_t charisma(string name);
 
     public:
     ledger(string ledger_path = "./build/character_ledger");
     ~ledger();
     void createCharacter();
-    void getCharacterInfo(string name);
+    void getCharacterAbilityScores(string name);
     void listCharacters();
+
 };
 
 #endif
