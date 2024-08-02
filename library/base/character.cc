ae6cb08efeca916c2d3a314063fd9c28ac1f1359
JREnglander <jenglander25@gmail.com>
2024-08-01 23:16:42 -0400

    Added fuctions to character for calculating skill and ability mods

--- library/base/character.cc
+++ library/base/character.cc
@@ -1,5 +1,12 @@
 #include "character.h"
 
+void Character::checkGivenIndex(size_t index, size_t max_array_value, string array_name)
+{
+    if (index >= max_array_value){
+        throw CharacterException("Received out of range index when accessing " + array_name);
+    }
+}
+
 Character::Character()
 {
 }
@@ -49,11 +56,13 @@ void Character::setShortName(string short_name)
 
 void Character::setAbilityScore(size_t index, int32_t value)
 {
+    checkGivenIndex(index, NUM_ABILITY_SCORES - 1, "the ability scores.");
     _ability_scores[index] = value;
 }
 
 int32_t Character::getAbilityScore(size_t index)
 {
+    checkGivenIndex(index, NUM_ABILITY_SCORES - 1, "the ability scores.");
     return _ability_scores[index];
 }
 
@@ -65,4 +74,68 @@ void Character::setBackstory(string backstory)
 string Character::getBackstory()
 {
     return _backstory;
+}
+
+void Character::setLevel(int level)
+{
+    _level = level;
+}
+
+int Character::getLevel()
+{
+    return _level;
+}
+
+int Character::getProficiencyBonus()
+{
+    RangeConverter *rc = new RangeConverter(MIN_LEVEL, 
+                                            MAX_LEVEL, 
+                                            MIN_PROFICIENCY_BONUS, 
+                                            MAX_PROFICIENCY_BONUS);
+    int bonus = rc->convertVal(_level);
+    delete rc;
+    return bonus;
+    
+}
+
+void Character::setProficiency(size_t index, bool val)
+{
+    checkGivenIndex(index, NUM_SKILLS - 1, "the proficiencies.");
+    _proficient_skills[index] = val;
+}
+
+bool Character::isProficient(size_t index){
+    checkGivenIndex(index, NUM_SKILLS - 1, "the proficiencies.");
+    return _proficient_skills[index];
+}
+
+void Character::setExpertise(size_t index, bool val)
+{
+    checkGivenIndex(index, NUM_SKILLS - 1, "the expertise.");
+    _expert_skills[index] = val;
+}
+
+bool Character::isExpert(size_t index)
+{
+    checkGivenIndex(index, NUM_SKILLS - 1, "the expertise.");
+    return _expert_skills[index];
+}
+
+int Character::getAbilityMod(size_t index)
+{
+    RangeConverter *rc = new RangeConverter(MIN_ABILITY_SCORE_VAL, 
+                                            MAX_ABILITY_SCORE_VAL, 
+                                            MIN_ABILITY_SCORE_MOD, 
+                                            MAX_ABILITY_SCORE_MOD);
+    int mod = rc->convertVal(getAbilityScore(index));
+    delete rc;
+
+    return mod;
+}
+
+int Character::getSkillMod(size_t index){
+    int mod = getAbilityMod(index);
+    if (isProficient(index)) mod += getProficiencyBonus();
+    if (isExpert(index)) mod += getProficiencyBonus();
+    return mod;
 }
