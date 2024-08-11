bd4901da6c8040a0da5679d3b5db906c212d9ca3
JREnglander <jenglander25@gmail.com>
2024-08-11 00:23:27 -0400

    Refactored options into groups and added options for character rolls

--- main.cc
+++ main.cc
@@ -39,8 +39,6 @@ string applyMod(int roll, int mod){
 }
 
 int main(int argc, char * argv[]){
-    // setting up the options
-    po::options_description desc("Options");
     bool adv_flag = false;
     bool dis_flag = false;
 
@@ -48,18 +46,36 @@ int main(int argc, char * argv[]){
     // annoying but thats how it is
     size_t num_options_with_defaults = 3; 
 
-    desc.add_options() 
-        ("help,h", "produce help message")
+    // setting up the options
+    po::options_description rollOpsDesc("Straight Roll Options");
+    rollOpsDesc.add_options()
         ("roll,r", po::value<int>(), "roll a dice with the provided max value")
         ("advantage,A", po::bool_switch(&adv_flag), "roll with advantage")
-        ("disadvantage,D", po::bool_switch(&dis_flag), "roll with disadvantage")
-        ("modifier,m", po::value<int>()->default_value(0), "add a modifier to the roll")
-        ("list_characters,l", "list names of saved characters.")
-        ("create_character,c", "start prompt to create a character.")
-        ("ability_scores,a", po::value<string>(), "list ability scores of a given character.")
-        ("character_bio,b",po::value<string>(), "get a the fluff for a character.")
-        ("delete_character,d", po::value<string>(), "remove a character from the ledger.");
+        ("disadvantage,v", po::bool_switch(&dis_flag), "roll with disadvantage")
+        ("modifier,m", po::value<int>()->default_value(0), "add a modifier to the roll");
+
+
+    po::options_description infoOpsDesc("Informational Options");
+    infoOpsDesc.add_options() 
+        ("help", "produce help message")
+        ("list", "list names of saved characters.")
+        ("create", "start prompt to create a character.")
+        ("ability_scores,A", po::value<string>(), "list ability scores of a given character.")
+        ("bio",po::value<string>(), "get a the fluff for a character.")
+        ("delete", po::value<string>(), "remove a character from the ledger.");
     
+    po::options_description characterRollOpsDesc("Character Roll Options");
+    characterRollOpsDesc.add_options()
+        ("strength,s", po::value<string>(), "roll with a strength mod")
+        ("dexterity,d", po::value<string>(), "roll with a dexterity mod")
+        ("intelligence,i", po::value<string>(), "roll with an intelligence mod")
+        ("wisdom,w", po::value<string>(), "roll with a wisdom mod")
+        ("charisma,c", po::value<string>(), "roll with a charisma mod")
+        ("constitution,C", po::value<string>(), "roll with a constitution mod");
+    
+    po::options_description desc;
+    desc.add(infoOpsDesc).add(rollOpsDesc).add(characterRollOpsDesc);
+
     po::variables_map vm;
     try {
         po::store(po::parse_command_line(argc, argv, desc), vm);
