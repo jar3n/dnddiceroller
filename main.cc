aa0c7668d5ca433f2ae1253626d7bc94bfbe2813
JREnglander <jenglander25@gmail.com>
2024-08-19 22:32:40 -0400

    removed short names and commas for commands

--- main.cc
+++ main.cc
@@ -34,11 +34,11 @@ int main(int argc, char * argv[]){
     // setting up the options
     po::options_description rollOpsDesc("Straight Roll Options");
     rollOpsDesc.add_options()
-        ("roll,r", po::value<int>(), "roll a dice with the provided max value")
-        ("advantage,A", po::bool_switch(&adv_flag), "roll with advantage")
-        ("disadvantage,D", po::bool_switch(&dis_flag), "roll with disadvantage")
-        ("modifier,m", po::value<int>()->default_value(0), "add a modifier to the roll")
-        ("multiple,M", po::value<int>()->default_value(1), "set number of times to roll");
+        ("roll", po::value<int>(), "roll a dice with the provided max value")
+        ("advantage", po::bool_switch(&adv_flag), "roll with advantage")
+        ("disadvantage", po::bool_switch(&dis_flag), "roll with disadvantage")
+        ("modifier", po::value<int>()->default_value(0), "add a modifier to the roll")
+        ("multiple", po::value<int>()->default_value(1), "set number of times to roll");
 
 
     po::options_description infoOpsDesc("Informational Options");
@@ -53,33 +53,33 @@ int main(int argc, char * argv[]){
     
     po::options_description abilityScores("Ability Score Mod Rolls");
     abilityScores.add_options()
-        ("strength,s", po::value<string>(), "roll with a strength mod")
-        ("dexterity,d", po::value<string>(), "roll with a dexterity mod")
-        ("intelligence,i", po::value<string>(), "roll with an intelligence mod")
-        ("wisdom,w", po::value<string>(), "roll with a wisdom mod")
-        ("charisma,c", po::value<string>(), "roll with a charisma mod")
-        ("constitution,C", po::value<string>(), "roll with a constitution mod");
+        ("strength", po::value<string>(), "roll with a strength mod")
+        ("dexterity", po::value<string>(), "roll with a dexterity mod")
+        ("intelligence", po::value<string>(), "roll with an intelligence mod")
+        ("wisdom", po::value<string>(), "roll with a wisdom mod")
+        ("charisma", po::value<string>(), "roll with a charisma mod")
+        ("constitution", po::value<string>(), "roll with a constitution mod");
     
     po::options_description skillMods("Skill Mod Rolls");
     skillMods.add_options()
         ("acrobatics", po::value<string>(), "roll an acrobatics check")
-        ("animal_handling,", po::value<string>(), "roll an animal handling check")
-        ("arcana,", po::value<string>(), "roll an arcana check")
-        ("athletics,", po::value<string>(), "roll an athletics check")
-        ("deception,", po::value<string>(), "roll a deception check")
-        ("history,", po::value<string>(), "roll a history check")
-        ("insight,", po::value<string>(), "roll an insight check")
-        ("intimidation,", po::value<string>(), "roll an intimidation check")
-        ("investigation,", po::value<string>(), "roll an investigation check")
-        ("medicine,", po::value<string>(), "roll a medicine check")
-        ("nature,", po::value<string>(), "roll a nature check")
-        ("perception,", po::value<string>(), "roll a perception check")
-        ("performance,", po::value<string>(), "roll a performance check")
-        ("persuasion,", po::value<string>(), "roll a persuasion check")
-        ("religion,", po::value<string>(), "roll a religion check")
-        ("sleight_of_hand,", po::value<string>(), "roll a sleight of hand check")
-        ("stealth,", po::value<string>(), "roll a stealth check")
-        ("survival,", po::value<string>(), "roll a survival check");
+        ("animal_handling", po::value<string>(), "roll an animal handling check")
+        ("arcana", po::value<string>(), "roll an arcana check")
+        ("athletics", po::value<string>(), "roll an athletics check")
+        ("deception", po::value<string>(), "roll a deception check")
+        ("history", po::value<string>(), "roll a history check")
+        ("insight", po::value<string>(), "roll an insight check")
+        ("intimidation", po::value<string>(), "roll an intimidation check")
+        ("investigation", po::value<string>(), "roll an investigation check")
+        ("medicine", po::value<string>(), "roll a medicine check")
+        ("nature", po::value<string>(), "roll a nature check")
+        ("perception", po::value<string>(), "roll a perception check")
+        ("performance", po::value<string>(), "roll a performance check")
+        ("persuasion", po::value<string>(), "roll a persuasion check")
+        ("religion", po::value<string>(), "roll a religion check")
+        ("sleight_of_hand", po::value<string>(), "roll a sleight of hand check")
+        ("stealth", po::value<string>(), "roll a stealth check")
+        ("survival", po::value<string>(), "roll a survival check");
 
     
     po::options_description characterRollOpsDesc("Character Roll Options");
